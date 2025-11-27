f361e66f1b30d268cd39c07636c633c66e4b6c9d
Khurraim1472 <khurraimbinfaisal@gmail.com>
2025-11-27 12:33:30 +0500

    practise arrays

--- Day4/Array.js
+++ Day4/Array.js
@@ -1,1 +1,75 @@
+// // Arrays
+// // let marks1 = 100;
+// // let marks2 = 65;
+// // let marks3 = 36;
+// // let marks4 = 78;
+
+
+// let marks = [100,65,36,78];
+// // console.log(marks);
+// // console.log(marks.length);
+
+// let arr = [100,30,"Hi",true];
+// // console.log(arr[2]);
+
+// arr[1] = 90;
+// // console.log(arr[1]);
+// // push:insert element at end of array
+// arr.push(5);
+// // arr.push("hello");
+// // console.log(arr);
+
+// // pop:delete element from end
+// arr.pop();
+// // console.log(arr);
+// // Unshift: Add Element in start, Shift:Delete element in start 
+// arr.unshift(44);
+// // console.log(arr);
+// arr.shift();
+// // console.log(arr);
+
+// let arr = [10,20,50,90,11];
+// let arr1 = arr;
+// arr1.push(22);
+// console.log(arr);
+
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+    
+// }
+// for(let num of arr) {
+//     console.log(num);
+    
+// }
+
+// const arr=[1,2,3,4];
+// arr =[3,7,9,6];
+// console.log(arr);
+
+// const arr=[1,2,3,4];
+// const arr2 = arr.slice(2,4);
+// console.log(arr2);
+// console.log(arr.splice(1,3));
+// console.log(arr);
+
+// spread operator
+// const arr1=[30,50,70,80,90];
+// const arr2=[40,60,45,55,66];
+// const arr3=[10,20,42,85,31];
+// const arr4=[...arr1,...arr2,...arr3];
+// console.log(arr4);
+
+
+const names = ["khurraim","hamza","messum","abbas"];
+// console.log(names.toString());
+// console.log(names.join("-"));
+// console.log(names.lastIndexOf("messum"));
+
+
+
+
+
+
+
+
 
