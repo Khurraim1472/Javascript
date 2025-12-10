efcb5f0b59a0669086dd88d4a9d5364704bf2d5f
Khurraim1472 <khurraimbinfaisal@gmail.com>
2025-12-10 12:13:12 +0500

    Operators practise

--- Day3/converson.js
+++ Day3/converson.js
@@ -11,14 +11,50 @@
 // true =>1 ;false=>0
  let isLoggedIn = 1;
  let BooleanIsLoggedIn = Boolean(isLoggedIn);
- console.log(BooleanIsLoggedIn);
+//  console.log(BooleanIsLoggedIn);
 //  1=>true, 0=>false,
 //  ""=>false; "hello"=> true
-
+  
 let number = 67;
 let stringNumber = String(number);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+            // ******Operations*****
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%5);
+
+
+let str1 = "hello";
+let str2 = " khurraim";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1"+ 2 );
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+  
 
  
 
