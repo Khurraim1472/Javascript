7eab58307e8a1a274bfca7488cc0488320ae12a8
Khurraim1472 <khurraimbinfaisal@gmail.com>
2025-12-10 11:56:00 +0500

    conversion of datatypes

--- Day3/converson.js
+++ Day3/converson.js
@@ -1,1 +1,26 @@
+// let score = "hello";
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+
+
+// "44" => 44
+// "44abc" => NaN
+// true =>1 ;false=>0
+ let isLoggedIn = 1;
+ let BooleanIsLoggedIn = Boolean(isLoggedIn);
+ console.log(BooleanIsLoggedIn);
+//  1=>true, 0=>false,
+//  ""=>false; "hello"=> true
+
+let number = 67;
+let stringNumber = String(number);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+ 
+
+
 
